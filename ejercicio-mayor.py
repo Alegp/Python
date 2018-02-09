34584889a6be6dbe15c607b0561d0c630da72d72
Alegp <alegp6@gmail.com>
2018-02-09 12:58:09 +0100

    Create ejercicio-mayor.py

---
+++ ejercicio-mayor.py
@@ -0,0 +1,12 @@
+#coding: utf8
+
+valor1 = input ("Escribe un numero: ")
+valor2 = input ("Escribe un numero: ")
+
+if valor1 > valor2:
+	print "El primer numero es mayor"
+else:
+    if valor2 > valor1:
+        print "El segundo es mayor"
+    else:
+		print "Los dos numeros son iguales"
