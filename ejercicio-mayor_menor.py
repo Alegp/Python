7ad467dc47b8155cadb8abf8a7dcb55f637ac0a6
Alegp <alegp6@gmail.com>
2018-02-09 12:58:34 +0100

    Create ejercicio-mayor_menor.py

---
+++ ejercicio-mayor_menor.py
@@ -0,0 +1,28 @@
+#coding: utf8
+
+variableA = input ("Escribe un numero: ")
+variableB = input ("Escribe un numero diferente: ")
+variableC = input ("Escribe un numero diferente: ")
+
+if (variableA == variableB) or (variableA == variableC) or (variableB == variableC):
+	print "Error: numero repetido"
+else:
+    if (variableA > variableB) and (variableA > variableC):
+		if (variableB > variableC):
+			print "Mayor variableA ,Menor variableB"
+		else:
+		    print "Mayor variableA ,Menor variableC"
+    else:
+	    if (variableB > variableA) and (variableB > variableC):
+		    if (variableA > variableC):
+			    print "Mayor variableB ,Menor variableC"
+		    else:
+		        print "Mayor variableB ,Menor variableA"
+	    else:
+	        if (variableC > variableA) and (variableC > variableB):
+		        if (variableA > variableB):
+			       print "Mayor variableC ,Menor variableB"
+		        else:
+		            print "Mayor variableC ,Menor variableA"
+	        else:
+	            pass
