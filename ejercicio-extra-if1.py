21b5b3f34a6bf6c93eb88db7b2becc8f4e2603af
Alegp <alegp6@gmail.com>
2018-02-12 21:50:45 +0000

    Create ejercicio-extra-if1.py

---
+++ ejercicio-extra-if1.py
@@ -0,0 +1,9 @@
+#coding: utf8
+
+dividendo = input ("Indique el dividendo: ")
+divisor = input ("Indique el divisor: ")
+
+if dividendo % divisor == 0:
+    print ("La division es exacta")
+else:
+	print ("La division no es exacta")
