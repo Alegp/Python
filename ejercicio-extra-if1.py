3ce8766440a79ff2c4241616d60e709a43fcfa6c
Alegp <alegp6@gmail.com>
2018-02-13 12:14:38 +0100

    Update ejercicio-extra-if1.py

--- ejercicio-extra-if1.py
+++ ejercicio-extra-if1.py
@@ -6,7 +6,10 @@ divisor   = input ("Escriba el divisor: ")
 cociente = dividendo // divisorç
 resto    = dividendo % divisor
 
+if divisor == 0:
+    print "No se puede dividir por 0"
+
 if dividendo % divisor == 0:
     print ("La division es exacta. Cociente: ", cociente)
 else:
-	print ("La division no es exacta. Cociente: ", cociente, ". Resto: ", resto)
+    print ("La division no es exacta. Cociente: ", cociente, ". Resto: ", resto)
