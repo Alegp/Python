4dd02c2842610960e2ed1f547a0b7c328b5ee20e
Alegp <alegp6@gmail.com>
2018-02-15 13:03:11 +0100

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+#coding: utf8
+
+num1 = input ("EScribe un numero:")
+operacion = raw_input ("Escribe un operador: ")
+num2 = input ("Escribe un numero: ")
+
+if not( (operacion == "+") or (operacion == "-") or (operacion == "*") or (operacion == "/") ):
+	print "Error: no has indicado un operador valido"
+	exit
+
+if (operacion == "+"):
+	print "El resultado es", (num1+num2)
+else:
+    if (operacion == "-"):
+        print "El resultado es", (num1-num2)
+    else:
+        if (operacion == "*"):
+            print "El resultado es", (num1*num2)
+        else:
+			      if (operacion == "/"):
+				        print "El resultado es" (num1/num2)
