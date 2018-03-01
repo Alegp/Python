24af7710eea14f75793b0914238a0650f27fe4e4
Alegp <alegp6@gmail.com>
2018-03-01 21:23:26 +0000

    Update calculo-area.py

--- calculo-area.py
+++ calculo-area.py
@@ -2,6 +2,7 @@
 
 import os
 os.system("clear")
+from math import pi
 
 print """
        Selecciona una forma:
@@ -9,15 +10,15 @@ print """
            b) Circulo
 """
 
-forma=raw_input (Indica una forma: )
+forma=raw_input ("Indica una forma: ")
 
-if (forma != a) or (forma != b):
+if (forma != "a") and (forma != "b"):
 	print "Error: esa opcion no existe"
 
-if (forma == a):
-	base = input (Indica la base: )
-	altura = input (Indica la altura: )
+if (forma == "a"):
+	base = input ("Indica la base: ")
+	altura = input ("Indica la altura: ")
 	print "Un triangulo de base", base, "y de altura", altura, "tiene un area de", ((base*altura)/2)
 else:
-	radio = input (Indica el radio: )
+	radio = input ("Indica el radio: ")
 	print "Un circulo de radio", radio, "tiene un area de", round(pi*radio*2,2)
