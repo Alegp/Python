8a4030d497e5b39c058ff470ff74bb35663e2598
Alegp <alegp6@gmail.com>
2018-03-02 13:02:50 +0100

    Create bucle-ondemand.py

---
+++ bucle-ondemand.py
@@ -0,0 +1,19 @@
+#coding: utf8
+
+# Inicializaciones
+salir = "N"
+numero = 1
+peticion = input("Escriba el numero limite: ")
+
+if peticion <= 0:
+	print "Error: No se puede 0 o negativos"
+else:
+	while ( salir=="N" ):
+		# Hago cosas
+		print numero
+
+		# Incremento
+		numero = numero + 1
+		# Activo indicador de salida si toca
+		if ( numero > peticion ): # Condición de salida
+				salir = "S"
