1eade99f19db5b1207993aaa2222885bd4c8c289
Alegp <alegp6@gmail.com>
2018-03-19 20:35:54 +0100

    Create bucle-sumar2.py

---
+++ bucle-sumar2.py
@@ -0,0 +1,21 @@
+# coding: utf8
+#Aleajandro Garía Ponce
+# Inicializaciones
+salir = "N"
+numero=1
+maximo=5
+suma=0
+while ( salir=="N" ):
+    # Hago cosas
+    if(numero%2==0):
+        print (numero )
+        if(numero<=maximo-2):
+            print ("+" )
+    
+        suma=suma+numero
+    # Incremento
+    numero=numero+1
+    # Activo indicador de salida si toca
+    if ( numero > maximo ): # Condición de salida
+            salir = "S"
+            print ("=" , suma )
