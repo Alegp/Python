919928cb960252b7323399078076a94630a91817
Alegp <alegp6@gmail.com>
2018-03-20 12:12:58 +0100

    Create plantilla-bucles.py

---
+++ plantilla-bucles.py
@@ -0,0 +1,17 @@
+#coding: utf8
+#Alejandro García Ponce
+# Inicializaciones
+
+
+salir = "N"
+(4)
+
+while ( salir=="N" ):
+    # Hago cosas
+    (2)
+
+    # Incremento
+    (3)
+    # Activo indicador de salida si toca
+    if (1): # Condición de salida
+        salir = "S"
